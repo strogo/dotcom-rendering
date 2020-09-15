3e5631d084be600b300413f53b04e6c3f7e8bb70
Jorge Azevedo <jorge.azevedo@guardian.co.uk>
2020-09-15 17:21:02 +0100

    Force banner

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,7 +1,7 @@
 module.exports = {
   ci: {
     collect: {
-      url: ['http://localhost:9000/Article?url=https://www.theguardian.com/commentisfree/2020/feb/08/hungary-now-for-the-new-right-what-venezuela-once-was-for-the-left#noads'],
+      url: ['http://localhost:9000/Article?url=https://www.theguardian.com/commentisfree/2020/feb/08/hungary-now-for-the-new-right-what-venezuela-once-was-for-the-left?force-remote-banner=true'],
       startServerCommand: 'NODE_ENV=production DISABLE_LOGGING_AND_METRICS=true node dist/frontend.server.js',
       numberOfRuns: '6',
       puppeteerScript: './scripts/lighthouse/puppeteer-script.js',
