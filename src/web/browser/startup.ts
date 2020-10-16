1534198fa4a644854e2baf31c7b308ffe72fe2fb
Tom Wey <tom.wey@theguardian.com>
2020-10-16 17:33:18 +0100

    Revert measure back to returning Promise<void>
    The end function now takes an optional flag and returns the time taken.
    Instead of passing directly to `then` in here, wrap it in an anonymous
    function call with the expected interface (takes no args and returns
    nothing), this fixes the type errors we saw previously.

--- src/web/browser/startup.ts
+++ src/web/browser/startup.ts
@@ -4,7 +4,7 @@ export interface Reporter {
     report: (err: Error, tags: { [key: string]: string }) => void;
 }
 
-const measure = (name: string, task: () => Promise<any>): void => {
+const measure = (name: string, task: () => Promise<void>): void => {
     const { start, end } = initPerf(name);
 
     start();
@@ -14,8 +14,12 @@ const measure = (name: string, task: () => Promise<any>): void => {
         // in Chrome 59 which is used by Cypress in headless mode so if you do it will
         // break the Cypress tests on CI
         // See: https://github.com/cypress-io/cypress/issues/2651#issuecomment-432698837
-        .then(end)
-        .catch(end);
+        .then(() => {
+            end();
+        })
+        .catch(() => {
+            end();
+        });
 };
 
 export const startup = <A>(
