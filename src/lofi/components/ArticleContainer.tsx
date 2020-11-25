7f2a95c0e3b63b78a0596ab2fed3c817df9abef9
Gareth <gtrufitt@gmail.com>
2020-11-25 16:18:35 +0000

    Remove unused files

--- src/lofi/components/ArticleContainer.tsx
+++ src/lofi/components/ArticleContainer.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { css, cx } from 'emotion';
-import { from, until } from '@guardian/src-foundations/mq';
-import { labelStyles } from '@root/src/lofi/components/AdSlot';
+import { until } from '@guardian/src-foundations/mq';
 
 const articleContainer = css`
     ${until.leftCol} {
@@ -17,70 +16,10 @@ const articleContainer = css`
     z-index: 1;
 `;
 
-const articleAdStyles = css`
-    .ad-slot {
-        width: 300px;
-        margin: 12px auto;
-        min-width: 160px;
-        min-height: 274px;
-        text-align: center;
-        position: relative;
-    }
-    .ad-slot--mostpop {
-        ${from.desktop} {
-            margin: 0;
-            width: auto;
-        }
-    }
-    .ad-slot--inline {
-        ${from.tablet} {
-            margin-right: -100px;
-            width: auto;
-            float: right;
-            margin-top: 4px;
-            margin-left: 20px;
-        }
-        ${from.desktop} {
-            width: auto;
-            float: right;
-            margin: 0;
-            margin-top: 4px;
-            margin-left: 20px;
-        }
-    }
-    .ad-slot--offset-right {
-        ${from.desktop} {
-            float: right;
-            width: auto;
-            margin-right: -318px;
-        }
-
-        ${from.wide} {
-            margin-right: -398px;
-        }
-    }
-    .ad-slot--outstream {
-        ${from.tablet} {
-            margin-left: 0;
-            width: 100%;
-
-            .ad-slot__label {
-                margin-left: 35px;
-                margin-right: 35px;
-            }
-        }
-    }
-    ${labelStyles};
-`;
-
 type Props = {
     children: JSXElements;
 };
 
 export const ArticleContainer = ({ children }: Props) => {
-    return (
-        <main className={cx(articleContainer, articleAdStyles)}>
-            {children}
-        </main>
-    );
+    return <main className={cx(articleContainer)}>{children}</main>;
 };
--- src/lofi/components/Onwards/HeadlinesContainer.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-type Props = {};
-
-export const HeadlinesContainer: React.FC<Props> = ({}: Props) => {
-    // need funky layout
-    return null;
-};
