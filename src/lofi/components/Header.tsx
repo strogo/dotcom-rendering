869a5bdc14995bb0489a6d01713c90a8d16550c4
Gareth <gtrufitt@gmail.com>
2020-11-25 11:41:01 +0000

    Scale back header

--- src/lofi/components/Header.tsx
+++ src/lofi/components/Header.tsx
@@ -1,10 +1,6 @@
 import React from 'react';
 import { css } from 'emotion';
-
-import { EditionDropdown } from '@frontend/web/components/EditionDropdown';
-import { Hide } from '@root/src/web/components/Hide';
 import { Logo } from '@frontend/web/components/Logo';
-import { Links } from '@frontend/web/components/Links';
 
 const headerStyles = css`
     /* Ensure header height contains it's children */
@@ -13,24 +9,8 @@ const headerStyles = css`
     -ms-overflow-style: none;
 `;
 
-type Props = {
-    edition: Edition;
-};
-
-export const Header = ({ edition }: Props) => (
+export const Header = () => (
     <header className={headerStyles}>
-        <Hide when="below" breakpoint="desktop">
-            <div id="edition-root">
-                <EditionDropdown
-                    edition={edition}
-                    dataLinkName="nav2 : topbar : edition-picker: toggle"
-                />
-            </div>
-        </Hide>
         <Logo />
-        <div id="reader-revenue-links-header" />
-        <div id="links-root">
-            <Links />
-        </div>
     </header>
 );
